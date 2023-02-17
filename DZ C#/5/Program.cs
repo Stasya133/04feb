5c1736cb515b4342847f1c78d86e0952c60f6d1f
Stasya133 <anastasiya_kl@mail.ru>
2023-02-17 23:26:40 +0300

    5

---
+++ DZ C#/5/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+456111 -> 5
+78 -> 8
+9146548 -> 1
+3 -> нет*/
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2=num;
+int count=0;
+while (num > 0)
+{
+    count++;
+    num = num / 10;
+}
+
+Console.WriteLine($"Разрядность введенного числа {count}");
+int res= num2;
+for (int i=1;i<=count;i++)
+    {
+        if (i!=count-0)
+            res = num2%10;
+        num2=num2/10;
+     
+    }
+        
+Console.WriteLine(res); 
+
+/*Console.WriteLine($"Разрядность введенного числа {count}");
+int res= num2; 
+int i=1;
+while (i<=count && num<10)
+       i++;  
+    {
+        if (i!=count-0)
+         {
+            res = num2%10;
+        num2=num2/10;
+    Console.WriteLine(res);
+         }
+else
+
+Console.WriteLine("{0} второй цифры нет ", num);
+
+ 
+    }*/
+
+
